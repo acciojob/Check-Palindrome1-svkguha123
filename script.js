7045d4e54c388c1d97c5b80600ce1e75891961de
Souvik Guha <117675254+svkguha123@users.noreply.github.com>
2023-03-15 14:22:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,17 @@
 
 function palindrome(str){
 
+	let start = 0;
+	let end = str.length-1;
+
+	while(start<=end) {
+		if(str[start] != str[end]) {
+			return false;
+			break;
+		}
+		start++;
+		end--;
+	}
+	return true;
 }
 module.exports = palindrome
